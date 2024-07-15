be11af0c47919233d42bee9d37bca57b2d9f5a76
Cha Youngeun <98088296+cye-dev@users.noreply.github.com>
2024-07-15 20:13:20 +0900

    Title: 행복한 수열의 개수, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240715/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,36 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = [
+    list(map(int, sys.stdin.readline().split()))
+    for _ in range(n)
+]
+ans = 0
+
+for row in range(n):
+    num = 0
+    cnt = 1
+    
+    for col in range(n):
+        if num == arr[row][col]:
+            cnt += 1
+        else:
+            num = arr[row][col]
+    
+    if cnt >= m:
+        ans += 1
+
+for col in range(n):
+    num = 0
+    cnt = 1
+    
+    for row in range(n):
+        if num == arr[row][col]:
+            cnt += 1
+        else:
+            num = arr[row][col]
+    
+    if cnt >= m:
+        ans += 1
+
+print(ans)
